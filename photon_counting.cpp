381eba6e1bb9694b2b3bee14da958c13c2555e01
Federico Settimo <fede.settimo@gmail.com>
2025-04-03 15:41:55 +0300

    Reduced noise in photon counting

--- photon_counting.cpp
+++ photon_counting.cpp
@@ -23,8 +23,10 @@ MatrixXcd projector (const VectorXcd &psi) {return psi*psi.adjoint();}
 //string label = "0"; // xi = 0
 //string label = "+"; // xi = .1
 string label = "-"; // xi = -1
-int dim_max = 2, Nensemble = 10000;
+int dim_max = 10, Nensemble = 10000;
 double gamma = 1., Omega = 1., phi = .2, nbar = .5, dt = .0005, tmax = 2.;
+// Possible values of xi
+double xi_0 = 0., xi_p = 0.01, xi_m = -1.;
 
 MatrixXd a () {
     MatrixXd aa = MatrixXd::Zero(dim_max, dim_max);
@@ -43,14 +45,22 @@ MatrixXcd H = .5*Omega*(a()*exp(-2.*I*phi) + adag()*exp(2.*I*phi));
 
 MatrixXcd J (const MatrixXcd &rho, double t) {
     double xi;
-    if (label == "0") xi = 0.;
-    else if (label == "+") xi = .01;
-    else xi = -1.;
+    if (label == "0") xi = xi_0;
+    else if (label == "+") xi = xi_p;
+    else xi = xi_m;
     return gamma*(nbar + 1.)*a()*rho*adag() + gamma*nbar*adag()*rho*a() + xi*(nbar + 1.)*a()*rho*adag();
 }
 
 MatrixXcd Gamma (double t) {
     return gamma*(nbar + 1.)*adag()*a() + gamma*nbar*a()*adag();
+}
+
+MatrixXcd Gamma_L (double t) {
+    double xi;
+    if (label == "0") xi = xi_0;
+    else if (label == "+") xi = xi_p;
+    else xi = xi_m;
+    return gamma*(nbar + 1.)*adag()*a() + gamma*nbar*a()*adag() + xi*(nbar + 1.)*adag()*a();
 }
 
 MatrixXcd L (const MatrixXcd &rho, double t) {
@@ -61,6 +71,23 @@ double observable_0 (const MatrixXcd &rho) {return real(rho(0,0));}
 double observable_1 (const MatrixXcd &rho) {return real(rho(1,1));}
 
 double rand01 () {return rand()/(double)RAND_MAX;}
+double max (double a, double b) {return a >= b ? a : b;}
+
+// Chooses where the jump happens
+VectorXcd jump (const MatrixXcd &R, double z) {
+    ComplexEigenSolver<MatrixXcd> eigs;
+    eigs.compute(R);
+    VectorXcd eigval = eigs.eigenvalues();
+    MatrixXcd eigvec = eigs.eigenvectors();
+
+    double sum_previous_eigs = 0.;
+    for (int j = 0; j < dim_max; ++j) {
+        if (z >= sum_previous_eigs*dt && z < (sum_previous_eigs + real(eigval(j)))*dt)
+            return eigvec.col(j)*exp(-arg(eigvec.col(j)[0]));
+        sum_previous_eigs += real(eigval(j));
+    }
+    return eigvec.col(dim_max-1)*exp(-arg(eigvec.col(dim_max-1)[0]));
+}
 
 int main () {
     srand(time(NULL));
@@ -95,44 +122,24 @@ int main () {
                 Nt++;
 
                 MatrixXcd R = J(projector(psi[i]), t);
-                // Compute all probabiities
-                double pdet = ((id - I*K*dt)*psi[i]).squaredNorm(), pj = real(R.trace())*dt;
-                double p_create = pdet +pj - 1., p_destroy = 1. - pdet - pj;
 
-                // First of all: should we destroy it?
-                if (p_destroy > 0) {
-                    double z = rand01();
-                    if (z > 1. - p_destroy)
-                        destroyed[i] = true;
-                    else if (z < pj) {
-                        ComplexEigenSolver<MatrixXcd> eigs;
-                        eigs.compute(R);
-                        VectorXcd eigval = eigs.eigenvalues();
-                        MatrixXcd eigvec = eigs.eigenvectors();
-                        if (z < real(eigval[0])*dt)
-                            psi[i] = eigvec.col(0);
-                        else psi[i] = eigvec.col(1);
-                    }
-                    else psi[i] = (id - I*K*dt)*psi[i];
+                // Compute all probabilities
+                double pj = real(R.trace())*dt,
+                    p_d = max(0, real(((Gamma(t) - Gamma_L(t))*projector(psi[i])).trace()) * dt),
+                    p_c = max(0, -real(((Gamma(t) - Gamma_L(t))*projector(psi[i])).trace()) * dt),
+                    pdet = 1. - pj - p_d - p_c;
+                double z = rand01();
+                if (z < pj) // Jump
+                    psi[i] = jump(R, z);
+                if (z >= pj && z < p_c + pj) { // Create a copy
+                    psi[i] = (id - I*K*dt)*psi[i];
+                    psi.push_back(psi[i]);
+                    destroyed.push_back(false);
                 }
-                else { // Or should we create a new copy?
-                    double z = rand01();
-                    if (z > 1. - p_create) {
-                        psi.push_back(psi[i]);
-                        destroyed.push_back(false);
-                    }
-                    z = rand01();
-                    if (z < real(R.trace())*dt) {
-                        ComplexEigenSolver<MatrixXcd> eigs;
-                        eigs.compute(R);
-                        VectorXcd eigval = eigs.eigenvalues();
-                        MatrixXcd eigvec = eigs.eigenvectors();
-                        if (z < real(eigval[0])*dt)
-                            psi[i] = eigvec.col(0);
-                        else psi[i] = eigvec.col(1);
-                    }
-                    else psi[i] = (id - I*K*dt)*psi[i];
-                }
+                else if (z >= p_c + pj && z < p_c + p_d + pj) // Destroy it
+                    destroyed[i] = true;
+                else // Det evo
+                    psi[i] = (id - I*K*dt)*psi[i];
                 psi[i].normalize();
             }
         }
--- photon_counting.py
+++ photon_counting.py
@@ -31,48 +31,15 @@ markevery = int(len(obs_0[0])/30)
 
 fig, ax = plt.subplots(1,1, figsize=(9,5), sharex=True, sharey=False)
 
-if False:
-    ax.plot(t, obs_0[0], color="red", label=r'$\zeta=0$', marker='o', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_0[1], '--', color="red", marker='o', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_avg_0[0], color="red", marker='o', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.plot(t, obs_avg_0[1], color="red", marker='o', fillstyle='none', markevery=markevery, linewidth=0)
-
-    ax.plot(t, obs_p[0], color="green", label=r'$\zeta=.01$', marker='x', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_p[1], '--', color="green", marker='x', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_avg_p[0], color="green", marker='x', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.plot(t, obs_avg_p[1], color="green", marker='x', fillstyle='none', markevery=markevery, linewidth=0)
-
-    ax.plot(t, obs_m[0], color="blue", label=r'$\zeta=-1$', marker='d', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_m[1], '--', color="blue", marker='d', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, obs_avg_m[0], color="blue", marker='d', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.plot(t, obs_avg_m[1], color="blue", marker='d', fillstyle='none', markevery=markevery, linewidth=0)
-
-    ax.legend(loc="upper left")
-    ax.set_ylabel(r'$\langle i\vert\rho_\zeta\vert i\rangle$')
-    ax.set_xlabel(r'$t$')
-    ax.set_title('Photon counting master equation')
-
-    size_inset = .2
-
-    axx = ax.inset_axes([.98-size_inset,.91-size_inset,size_inset,size_inset])
-    axx.plot(t, trace_0[0], color='red')
-    axx.plot(t, trace_0[1], color='red', marker='o', fillstyle='none', markevery=markevery*3, linewidth=0)
-    axx.plot(t, trace_p[0], color='green')
-    axx.plot(t, trace_p[1], color='green', marker='x', fillstyle='none', markevery=markevery*3, linewidth=0)
-    axx.plot(t, trace_m[0], color='blue')
-    axx.plot(t, trace_m[1], color='blue', marker='d', fillstyle='none', markevery=markevery*3, linewidth=0)
-    axx.set_ylabel(r'$\operatorname{tr}[\rho_\zeta]$')
-    axx.set_xticks([])
-else:
-    ax.plot(t, trace_0[0], color='red', label=r'$\zeta=0$', marker='o', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, trace_0[1], color='red', marker='o', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.plot(t, trace_p[0], color='green', label=r'$\zeta=.01$', marker='x', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, trace_p[1], color='green', marker='x', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.plot(t, trace_m[0], color='blue', label=r'$\zeta=-1$', marker='d', fillstyle='none', markevery=len(obs_0[0]))
-    ax.plot(t, trace_m[1], color='blue', marker='d', fillstyle='none', markevery=markevery, linewidth=0)
-    ax.set_ylabel(r'$\operatorname{tr}[\rho_\zeta]$')
-    ax.legend(loc="lower left")
-    ax.set_xlabel(r'$t$')
-    ax.set_title('Photon counting master equation')
+ax.plot(t, trace_0[0], color='red', label=r'$\zeta=0$', marker='o', fillstyle='none', markevery=len(obs_0[0]))
+ax.plot(t, trace_0[1], color='red', marker='o', fillstyle='none', markevery=markevery, linewidth=0)
+ax.plot(t, trace_p[0], color='green', label=r'$\zeta=.01$', marker='x', fillstyle='none', markevery=len(obs_0[0]))
+ax.plot(t, trace_p[1], color='green', marker='x', fillstyle='none', markevery=markevery, linewidth=0)
+ax.plot(t, trace_m[0], color='blue', label=r'$\zeta=-1$', marker='d', fillstyle='none', markevery=len(obs_0[0]))
+ax.plot(t, trace_m[1], color='blue', marker='d', fillstyle='none', markevery=markevery, linewidth=0)
+ax.set_ylabel(r'$\operatorname{tr}[\rho_\zeta]$')
+ax.legend(loc="lower left")
+ax.set_xlabel(r'$t$')
+#ax.set_title('Photon counting master equation')
 
 plt.show()
